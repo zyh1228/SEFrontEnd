cd77b60c58393e725466ebd119621bdddb04cb88
zyh1228 <unknown@example.com>
2022-06-21 17:28:29 +0800

    添加功能完成

--- pages/add/index.js
+++ pages/add/index.js
@@ -1,66 +1,129 @@
-// pages/add/index.js
-Page({
+import api from '../api'
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+const app = getApp()
 
+
+Page({ 
+  data: {   
+    modelFile: null, 
+    image: null,
+    allowUploadModel: false,
+    objModelId: null,
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+  //上传图片到服务器 
+  addModelInfo:function (e) { 
+    var that = this  
+    var img = that.data.image
 
+    var name = e.detail.value.biaoti 
+    var description = e.detail.value.neirong
+    var category = e.detail.value.zuozhe
+
+    if (!(name && description && category)) {
+      return
+    }
+
+    api.addModel((res)=>{
+      console.log(res)
+      that.setData({
+        objModelId: res.id,
+        allowUploadModel: true
+      })
+    }, name, description, category, img)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  //上传文件到服务器 
+  formSubmit:function (e) { 
+    var that = this  
+    
+    if (!that.data.modelFile) {
+      return
+    }
+    api.upLoadModel((res)=>{
+      wx.showToast({
+        title: '添加成功',
+        icon: 'success',
+        duration: 2000
+      })
+      wx.navigateBack({
+        delta: 1
+      })    
+    }, that.data.objModelId, that.data.modelFile.path)
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  //从本地获取照片 
+  upimg: function () {  
+    var that = this;  
+    wx.chooseImage({    
+      count: 1,        //一次性上传到小程序的数量
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+        that.setData({      
+          image: res.tempFilePaths[0],
+        })
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
+  //删除照片功能与预览照片功能 
+  deleteImg: function (e) {  
+    var that = this;
+    wx.showModal({
+      title: '提示',
+      content: '确定要删除此图片吗？',
+      success: function (res) {
+        if (res.confirm) {
+          that.setData({
+            image: unll
+          });
+        } else if (res.cancel) {
+          return false;
+        }
+      }    
+    })  
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
+  previewImg: function (e) {
+    wx.previewImage({
+      urls: this.data.image    
+    })
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
+  //从聊天记录获取文件
+  upModel: function () {  
+    var that = this;  
+    wx.chooseMessageFile({    
+      count: 1,        //一次性上传到小程序的数量
+      type: 'file',
+      success(res) {
+        that.setData({      
+          modelFile: res.tempFiles[0],
+        })
+        console.log(res)
+      }
+    })
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
+  //删除文件功能 
+  deleteModel: function (e) {  
+    var that = this;
+    if (!that.data.modelFile) {
+      return false
+    }
+    wx.showModal({
+      title: '提示',
+      content: '确定要删除此文件吗？',
+      success: function (res) {
+        if (res.confirm) {
+          that.setData({
+            modelFile: unll
+          });
+        } else if (res.cancel) {
+          return false
+        }
+      }    
+    })  
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
--- pages/api.js
+++ pages/api.js
@@ -1,6 +1,6 @@
 var app = getApp()
 
-function baseRequest(url, method, callback, options, contentType) {
+function baseRequest(url, method, callback, options, contentType, fileKey, filePath) {
   if (options !== undefined) {
     var {params = {}, data = {}} = options
   } else {
@@ -19,31 +19,74 @@ function baseRequest(url, method, callback, options, contentType) {
 
   console.log(url)
 
-  wx.request({
-    url: url,
-    method: method, 
-    data: data, 
-    header: { 
-      'Content-Type': contentType,
-      'cookie': wx.getStorageSync('Set-Cookie')
-    }, 
-    success: function(res) { 
-      if (res.data.error === null) {
-        callback(res.data.data)
+  if (filePath === undefined){
+    wx.request({
+      url: url,
+      method: method, 
+      data: data, 
+      header: { 
+        'Content-Type': contentType,
+        'cookie': wx.getStorageSync('Set-Cookie')
+      }, 
+      success: function(res) { 
+        if (res.data.error === null) {
+          callback(res.data.data)
+        } else {
+          wx.showToast({
+            title: res.data.data,
+            icon: 'fail',
+            duration: 2000
+          })
+        }
         if (res.header['Set-Cookie'] != '') {
           wx.setStorageSync('Set-Cookie', res.header['Set-Cookie'])
         }
-      }
-    } ,
-    fail: function(err) { //请求失败
-      //callback('error')
-      wx.showToast({
-        title: '请求失败',
-        icon: 'fail',
-        duration: 2000
-      })
-    },
-  })
+      },
+      fail: function(err) { //请求失败
+        //callback('error')
+        wx.showToast({
+          title: '请求失败',
+          icon: 'fail',
+          duration: 2000
+        })
+      },
+    })
+  } else {
+    wx.uploadFile({
+      url: url,
+      name: fileKey,
+      filePath: filePath,
+      method: method, 
+      formData: data, 
+      header: { 
+        'cookie': wx.getStorageSync('Set-Cookie')
+      }, 
+      success: function(res) {
+        let result = JSON.parse(res.data)
+        if (!result.error) {
+          callback(result.data)
+        } else {
+          wx.showToast({
+            title: result.data,
+            icon: 'fail',
+            duration: 2000
+          })
+        }
+        if (res.header['Set-Cookie'] != '') {
+          wx.setStorageSync('Set-Cookie', res.header['Set-Cookie'])
+        }
+      },
+      fail: function(err) { //请求失败
+        //callback('error')
+        console.log(err)
+        wx.showToast({
+          title: '请求失败',
+          icon: 'fail',
+          duration: 2000
+        })
+      },
+    })
+  }
 }
 
 function userLogin(success, code, nickname, avatarURL) {
@@ -82,6 +125,22 @@ function deleteHistory(success, id) {
   baseRequest('http://' + app.globalData.HOST + '/api/history/history', 'DELETE', success, {params})
 }
 
+function addModel(success, name, description, category, coverImage) {
+  let data = {
+    'name': name,
+    'description': description,
+    'category': category,
+  }
+  baseRequest('http://' + app.globalData.HOST + '/api/model/obj', 'POST', success, {data}, undefined, "cover", coverImage)
+}
+
+function upLoadModel(success, id, modelFile) {
+  let data = {
+    'id': id
+  }
+  baseRequest('http://' + app.globalData.HOST + '/api/model/obj/uploadModel', 'POST', success, {data}, undefined, "model", modelFile)
+}
+
 module.exports = {
   userLogin,
   getCategory,
@@ -89,4 +148,6 @@ module.exports = {
   getModelDetail,
   getHistoryList,
   deleteHistory,
+  addModel,
+  upLoadModel
 }
